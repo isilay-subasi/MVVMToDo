7557cafa298d015220efbd30e3f8ee4a9feef685
isilay-subasi <isil.subasi19@gmail.com>
2022-02-24 19:06:21 +0300

    ViewModel adapter

--- app/src/main/java/com/example/todoapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/tasks/TasksFragment.kt
@@ -1,13 +1,38 @@
 package com.example.todoapp.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentTaskBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_task) {
 
     private val viewModel : TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTaskBinding.bind(view)
+
+        val taskAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter=taskAdapter
+                layoutManager=LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner){
+            taskAdapter.submitList(it)
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/todoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/tasks/TasksViewModel.kt
@@ -2,6 +2,7 @@ package com.example.todoapp.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.example.todoapp.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,4 +11,7 @@ import javax.inject.Inject
 class TasksViewModel @Inject constructor(
     private val taskDao : TaskDao
 ) : ViewModel(){
+
+    val tasks = taskDao.getTasks().asLiveData()
+
 }
